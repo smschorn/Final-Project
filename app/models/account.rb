8eee6f1efa312087506e3f7673db52c766a6d7f7
SCOTT SCHORN <smschorn@gmail.com>
2018-04-12 14:11:41 -0400

    added devise and model relations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,4 @@
 class Account < ApplicationRecord
+  belongs_to :user
+  has_many :account_statements
 end
--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -1,2 +1,4 @@
 class Ledger < ApplicationRecord
+  belongs_to :user
+  has_many :account_statements
 end
