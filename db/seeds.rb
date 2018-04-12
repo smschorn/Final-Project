e2a46b7b22d6a862d4c8287bff3d2fb038954e14
SCOTT SCHORN <smschorn@gmail.com>
2018-04-12 14:26:53 -0400

    styled devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(email: 'test@email.com', password: 'password', password_confirmation: 'password')
+
+accounts = [
+  Account.create(user: user, name: 'AmEx', interest_rate: 1.50),
+  Account.create(user: user, name: 'BofA MC', interest_rate: 1.50),
+  Account.create(user: user, name: 'BofA VISA', interest_rate: 1.50),
+]
+
+ledger = Ledger.create(user: user, month: 3, year: 2018, budget: 790.93)
+
+AccountStatement.create(ledger: ledger, account: accounts[0], )
+
+AccountStatement.create(ledger: ledger, account: accounts[0], current_balance: 5452.84,  minimum_payment: 209.00)
+AccountStatement.create(ledger: ledger, account: accounts[1], current_balance: 0,        minimum_payment: 25.00)
+AccountStatement.create(ledger: ledger, account: accounts[2], current_balance: 2308.94,  minimum_payment: 88.00)
