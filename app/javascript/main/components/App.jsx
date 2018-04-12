7bcff6f1717bdde523eedee7fd34af2331d31226
SCOTT SCHORN <smschorn@gmail.com>
2018-04-12 15:58:15 -0400

    added react app

---
+++ app/javascript/main/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'reactstrap';
+
+const App = (props) => (
+  <div>
+    <Container fluid>
+      <h1>Hello</h1>
+    </Container>
+  </div>
+)
+
+export default App;
