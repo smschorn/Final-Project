9432601a165c33f2f8aca1907134f44ad9d4781a
SCOTT SCHORN <smschorn@gmail.com>
2018-04-12 14:43:30 -0400

    added accounts api

---
+++ app/controllers/api/accounts_controller.rb
@@ -0,0 +1,41 @@
+class Api::AccountsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  before_action :set_account, only: [:update, :destroy]
+  respond_to :json
+
+  # GET /api/accounts.json
+  def index
+    @accounts = current_user.accounts.all
+    respond_with @accounts
+  end
+
+  # POST /api/accounts.json
+  def create
+    @account = current_user.accounts.create(account_params)
+    respond_with @account, location: api_account_url(@account)
+  end
+
+  # PATCH/PUT /api/accounts/1.json
+  def update
+    @account.update(account_params)
+    respond_with @account, json: @account
+  end
+
+  # DELETE /api/accounts/1.json
+  def destroy
+    @account.destroy
+    respond_with @account
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_account
+      @account = current_user.accounts.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def account_params
+      params.require(:account).permit(:name, :description, :interest_rate, :user_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :api, format: :json do
+    resources :accounts, only: [:index, :create, :update, :destroy]
+  end
   root to: 'react#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
