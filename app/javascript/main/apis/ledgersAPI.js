4253b6b841ce19b7e69782ba1c192bbd3f420562
SCOTT SCHORN <smschorn@gmail.com>
2018-04-15 09:33:15 -0400

    added moment to ledgers

--- app/javascript/main/apis/ledgersAPI.js
+++ app/javascript/main/apis/ledgersAPI.js
@@ -1,31 +1,57 @@
 import axios from 'axios';
+import moment from 'moment';
+
+const transformLedger = function(data) {
+  data.date = moment({year: data.year, month: data.month - 1})
+  return data;
+}
+
+const transformLedgers = function(data) {
+  return data.map(transformLedger)
+}
 
 const ledgersAPI = {
   getAll: function() {
-    return axios.get('api/ledgers.json')
+    return axios({
+      url: '/api/ledgers.json',
+      transformResponse: [], concat(axios.defaults.transformResponse, transformLedgers)
+    });
   },
 
-  getOne: function(ledgerId) {
-      return axios.get(`api/ledgers/${ledgerId}.json`);
+  getOne: function(id) {
+    return axios({
+      url: `/api/ledgers/${id}.json`,
+      transformResponse: [], concat(axios.defaults.transformResponse, transformLedger)
+    });
   },
 
   create: function(ledger) {
-    return axios.post('api/ledgers.json', {
-      ledger: {
-        year: ledger.year,
-        month: ledger.month,
-        budget: ledger.budget,
-      }
-    })
+    return axios({
+      url: '/api/ledgers.json',
+      method: 'post',
+      data: {
+        ledger: {
+          month: ledger.month,
+          year: ledger.year,
+          budget: ledger.budget,
+        }
+      },
+      transformResponse: [], concat(axios.defaults.transformResponse, transformLedger)
+    });
   },
 
   update: function(ledger) {
-    return axios.put(`api/ledgers/${ledger.id}.json`, {
-      ledger: {
-        year: ledger.year,
-        month: ledger.month,
-        budget: ledger.budget,
-      }
+    return axios({
+      url: `/api/ledgers/${ledger.id}.json`,
+      method: 'patch',
+      data: {
+        ledger: {
+          month: ledger.month,
+          year: ledger.year,
+          budget: ledger.budget,
+        }
+      },
+      transformResponse: [], concat(axios.defaults.transformResponse, transformLedger)
     });
   },
 
